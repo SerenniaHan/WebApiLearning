e4fdbff27e0777086859418f708a19e834a92ad0
serennia <yihookan@yahoo.co.jp>
2024-06-23 17:45:05 +0900

    Implement Edit form value validation

--- WebApiLearning.WebApp/Models/GameDetails.cs
+++ WebApiLearning.WebApp/Models/GameDetails.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApiLearning.WebApp.Models;
 
 public class GameDetails
 {
+    [Required] 
+    [StringLength(50)]
     public required string Name { get; set; }
+
+    [Required(ErrorMessage = "The genre field is required.")] 
     public string? GenreId { get; set; }
+    
+    [Range(1, 100000)]
     public required decimal Price { get; set; }
     public DateOnly ReleaseDate { get; set; }
 }
